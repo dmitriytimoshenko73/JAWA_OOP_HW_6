29c80555cb42ace6c30cce4a3217907833e30195
dmitriytimoshenko73 <dmtimoshenko@mail.ru>
2024-02-19 17:17:44 +0300

    added changed

--- Main.java
+++ Main.java
@@ -4,20 +4,20 @@ import controller.TeacherControler;
 public class Main {
     public static void main(String[] args) {
         StudentControler controler = new StudentControler();
-        controler.create("Анатолий", "Петрович", "Иванов");
-        controler.create("Аркадий", "Семенович", "Петров");
-        controler.create("Семен", "Олегович", "Сидоров");
-        controler.create("Виктор", "Иванович", "Агеев");
+        controler.create("Виктор", "Петрович", "Викторов");
+        controler.create("Арсен", "Семенович", "Петрян");
+        controler.create("Густав", "Олегович", "Суркин");
+        controler.create("Вениамин", "Иванович", "Маркин");
         
         controler.sendOnConsole();
         System.out.println();
 
 
         TeacherControler teacherControler = new TeacherControler();
-        teacherControler.create("Татьяна", "Петровна", "Семёнова");
-        teacherControler.create("Игорь", "Семёнович", "Воронин");
-        teacherControler.create("Антонина", "Георгиевна", "Сомова");
-        teacherControler.create("Марат", "Эрнстович", "Танагузов");
+        teacherControler.create("Изольда", "Петровна", "Семёнова");
+        teacherControler.create("Иван", "Семёнович", "Пупкин");
+        teacherControler.create("Агрипина", "Георгиевна", "Карасева");
+        teacherControler.create("Махмуд", "Эрнстович", "Шильман");
         teacherControler.sendOnConsole();
         
         
